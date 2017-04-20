9d6701bbfd44c39e8d5335f34c2604cc9c7b3ebe
Anessa Ortner <anessa.ortner@gmail.com>
2017-04-20 18:54:50 -0500

    base index setup and js file setting input values

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
 console.log("working js file");
+
+$("#fight-btn").click(() => {
+	const player1 = $("#player1").val();
+	const player2 = $("#player2").val();
+	console.log("player 1: ", player1);
+	console.log("player2: ", player2);
+});
